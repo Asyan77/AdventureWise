b99f9c493a93a1dbee5dd9dca6a0e76d79e1862f
Ashley Yan <Asyan77@gmail.com>
2023-07-20 23:30:36 -0700

    add heading, buttons, some CSS

---
+++ FoodAroundTheWorld/src/scripts/script.js
@@ -0,0 +1,13 @@
+const map = L.map('map').setView([0, 0], 2);
+
+// Add a tile layer (you can choose a different tile provider if you like)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// Add a marker (You can customize the marker icon and position based on your needs)
+L.marker([51.505, -0.09]).addTo(map)
+    .bindPopup("London, UK")
+    .openPopup();
+
+L.marker([40.7128, -74.0060]).addTo(map)
+    .bindPopup("New York City, USA")
+    .openPopup();
